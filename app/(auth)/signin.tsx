7ab7ad628debac597f07b2203f77d79c95490755
morachake <jacobmoracha@gmail.com>
2025-03-27 11:21:28 +0300

    getting started

---
+++ app/(auth)/signin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {SafeAreaView,Text,View} from 'react-native'
+
+
+export default function SignIn () {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Logo</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
